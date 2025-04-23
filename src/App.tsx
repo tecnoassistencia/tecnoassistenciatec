5e7e2d5278c40b7e463064f36795271c7387bbf5
tecnoassistencia <lucaschristussul@gmail.com>
2025-04-23 10:49:54 -0300

    App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à Tecno Assistência!</h1>
+      <p>Seu serviço de assistência técnica de computadores. Acompanhe o status do seu equipamento em tempo real.</p>
+    </div>
+  );
+};
+
+export default App;
