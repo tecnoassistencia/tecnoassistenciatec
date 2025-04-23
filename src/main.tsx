974edfb16ae11ce27bffb26172c599c84c47c09c
tecnoassistencia <lucaschristussul@gmail.com>
2025-04-23 11:12:59 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Certifique-se de que você está usando React 18+
-import App from './App';  // O componente App que você vai criar ou já tem
-import './index.css';     // Caso você tenha algum arquivo CSS para estilizar o projeto
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement); // Criação do ponto de entrada
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("app")
 );
